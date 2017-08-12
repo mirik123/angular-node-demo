a603ed788fe37c962eb24b5c5c2b0d8af6d1cb10
Mark Babayev <markolog@gmail.com>
2017-08-12 12:10:51 +0300

    fix bugs

--- node-http-mongodb-es6/server/db/authdb.ts
+++ node-http-mongodb-es6/server/db/authdb.ts
@@ -107,7 +107,7 @@ export class DB {
         return DB.records.remove({}).exec();
     }
     
-    static findall(): Promise<Record[]> {
+    static findall(): Promise<any[]> {
         return DB.records.find({}).lean().exec();
     }
 
